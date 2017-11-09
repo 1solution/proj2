0a594612cf0d6ba99c88c0968ec3a47aae769054
Mikee <Michal.Plsek@seznam.cz>
2017-11-09 21:20:13 +0100

    Update proj2.c

--- proj2.c
+++ proj2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 	double myOwnPow(double x, int n) { // fce simuluje mocninu
 		int naKolikatou = 2*n - 1;
@@ -22,42 +23,95 @@
 	return vysledek;
 	}
 
-	int eps(double uhel) {
+	int eps(double uhel) { // fce zpresnuje vysledek na 10 mist, mezi n a n+1
     int n = 1;
     while(fabs(cfrac_tan(uhel,n) - cfrac_tan(uhel,n+1)) > 0.00000000001)
       n++;
     return n;
 	}
 
-  struct Vysledky dopocitani(double alfa, double beta, double c) {
-
-        double delka = c / cfrac_tan(alfa,eps(alfa)); // vyp delky
-        double vyska = c + (delka * cfrac_tan(beta,eps(beta))); // vyp vysky
-        struct Vysledky hold = {delka, vyska};
-      return hold;
+  double vypocetDelka(double alfa, double c) { // vypocet D
+        double delka = c / cfrac_tan(alfa,eps(alfa));
+			return delka;
+	}
+ double vypocetVyska(double alfa, double beta, double c) { // vypocet V
+        double vyska = c + (vypocetDelka(alfa, c) * cfrac_tan(beta,eps(beta)));
+      return vyska;
 	}
 
-int main(int argc, char *argv[]) {
-
-    struct Vysledky {
-    double delka;
-    double vyska;
-    };
-
-	double tt = 1.024; // nacti argv1
-  int N = 2; // nacti argv 2
-  int M = 7; // nacti argv3
-  double sumTaylor = 0;
-  double alfa = 0.3;
-  double beta = 1.3;
-  double c = 1.5;
-
-	for(int i = 1; i <= M; i++) {
-		sumTaylor += taylor_tan(tt,i);
-		if(i >= N && i <= M) {// TADY TO VEZME KAZDY KROK MEZI N A M, tzn tisk provadet asi tady
-			printf("%d %e %e %e %e %e\n", i, tan(tt), sumTaylor, fabs(tan(tt) - sumTaylor), cfrac_tan(tt,i), fabs(tan(tt)-cfrac_tan(tt,i)));
+		void tiskniTan(int* N, int* M, double* tt) {
+		double sumTaylor = 0;
+		for(int i = 1; i <= *M; i++) {
+			sumTaylor += taylor_tan(*tt,i);
+			if(i >= *N && i <= *M)
+				printf("%d %e %e %e %e %e\n", i, tan(*tt), sumTaylor, fabs(tan(*tt) - sumTaylor), cfrac_tan(*tt,i), fabs(tan(*tt)-cfrac_tan(*tt,i)));
 		}
 	}
 
+int main(int argc, char **argv) {
+
+  double c = 1.5; // default c = 1.5
+
+  if(argv[1]) {
+    if(strcmp("--help",argv[1]) == 0 && argc == 2) {
+      char napoveda[] = "--tan A N M, where A is RAD angle (0;1.4>, N is start cycle and M is end cycle\n[-c X] -m A [B], where X is height of measurement starting point, A is RAD angle (0;1.4> and B is RAD angle (0;1.4>";
+      printf("%s", napoveda);
+      return 0;
+    }
+    else if(strcmp("--tan",argv[1]) == 0 && argc == 5) { // pak over jestli to jsou cisla. VSUDE. podminka pro uhel min a max (0;1.4> isinf a isnan
+      double tt = atof(argv[2]);
+      int N = atoi(argv[3]);
+      int M = atoi(argv[4]);
+        if(0 < N && N <= M && M < 14)
+          tiskniTan(&N, &M, &tt);
+        else {
+        fprintf(stderr, "%s", "Incorrect N / M values (type --help)");
+        return 1;
+        }
+
+      return 0;
+    }
+    else if(strcmp("-c",argv[1]) == 0) {
+      c = atof(argv[2]);
+      if(c > 0 && c <= 100) {
+        if(argc == 5) {
+          double alfa = atof(argv[4]);
+          printf("%.10e\n", vypocetDelka(alfa,c));
+          return 0;
+        }
+        if(argc == 6) {
+          double alfa = atof(argv[4]);
+          double beta = atof(argv[5]);
+          printf("%.10e\n%.10e\n", vypocetDelka(alfa,c), vypocetVyska(alfa,beta,c));
+          return 0;
+        }
+      }
+      else {
+      fprintf(stderr, "%s", "Incorrect X value (type --help)");
+      return 1;
+      }
+    }
+    else if(strcmp("-m",argv[1]) == 0) {
+      if(argc == 3) {
+        double alfa = atof(argv[2]);
+        printf("%.10e\n", vypocetDelka(alfa,c));
+        return 0;
+      }
+      if(argc == 4) {
+        double alfa = atof(argv[2]);
+        double beta = atof(argv[3]);
+        printf("%.10e\n%.10e\n", vypocetDelka(alfa,c), vypocetVyska(alfa,beta,c));
+        return 0;
+      }
+    }
+  }
+  else {
+    fprintf(stderr, "%s", "No argument entered.\n");
+    return 1;
+  }
+
 return 0;
+
+system("PAUSE");
+
 }
