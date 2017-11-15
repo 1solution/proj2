3649058e5df068729046ec55dda180f68c43edb9
Mikee <Michal.Plsek@seznam.cz>
2017-11-15 13:35:47 +0100

    Update proj2.c

--- proj2.c
+++ proj2.c
@@ -1,29 +1,16 @@
+// xplsek03, M.Plsek
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <ctype.h>
 #define EPS 0.00000000005
-// xplsek03, M.Plsek
+
 	int checkAngle(double x) { // checks angle value from (0; 1.4>
 		if(x > 0 && x <= 1.4)
 			return 1;
 		return 0;
 	}
-/*
-	int validArguments(int argc, char* argv[]) { // fction checks if arguments put by user are in valid format
-	  int override = 0;
-		for(int i = 2; i < argc; i++) {
-		  if(argc > 4 && !strcmp(argv[1],"-c") && !strcmp(argv[3],"-m"))
-        override = 1;
-			for(int k = 0; argv[i][k] != '\0'; k++) {
-				if(!isdigit(argv[i][k]) && argv[i][k] != '.' && argv[i][k] != 'e' && override == 0)
-					return 0;
-			}
-		}
-		return 1;
-	}
-*/
 	double taylor_tan(double x, unsigned int n) { // processing taylor_tan step by step
 		unsigned long long citatel[] = {1, 1, 2, 17, 62, 1382, 21844, 929569, 6404582, 443861162, 18888466084, 113927491862, 58870668456604};
 		unsigned long long jmenov[] = {1, 3, 15, 315, 2835, 155925, 6081075, 638512875, 10854718875, 1856156927625, 194896477400625, 49308808782358125, 3698160658676859375};
@@ -42,14 +29,12 @@
 			vysledek = 1 / (((2*i - 1) / x) - vysledek);
 		return vysledek;
 	}
-
 	int eps(double uhel) { // eps function , 10 dec places between n and n+1
     int n = 1;
     while(fabs(cfrac_tan(uhel,n) - cfrac_tan(uhel,n+1)) > EPS)
       n++;
     return n;
 	}
-
   double vypocetDelka(double alfa, double c) { // calculate length D
         double delka = c / cfrac_tan(alfa,eps(alfa));
 			return delka;
@@ -58,41 +43,37 @@
         double vyska = c + (vypocetDelka(alfa, c) * cfrac_tan(beta,eps(beta)));
       return vyska;
 	}
-
-		void tiskniTan(int* N, int* M, double* tt) { // prints --tan output
+		int tiskniTan(int* N, int* M, double* tt) { // prints --tan output
 		for(int i = 1; i <= *M; i++) {
-			if(i >= *N && i <= *M)
+			if(i >= *N && i <= *M) {
 				printf("%d %e %e %e %e %e\n", i, tan(*tt), taylor_tan(*tt,i), fabs(tan(*tt) - taylor_tan(*tt,i)), cfrac_tan(*tt,i), fabs(tan(*tt)-cfrac_tan(*tt,i)));
+			}
+			else return 0;
 		}
+		return 1;
 	}
 
 int main(int argc, char **argv) {
-
-	/*if(!validArguments(argc, argv)) {
-		printf("Incorrect argument input (type --help)");
-		return 1;
-	}*/
   double c = 1.5;
 
   if(argc > 1) {
-
     if(!strcmp("--help",argv[1]) && argc == 2) {
-      char napoveda[] = "--tan A N M, where A is RAD angle (0;1.4>, N is start cycle and M is end cycle (0 < N <= M < 14)\n[-c X] -m A [B], where X is height of measurement starting point, A is RAD angle (0;1.4> and B is RAD angle (0;1.4>";
+      char napoveda[] = "--tan A N M, where A is RAD angle, N is start cycle and M is end cycle (0 < N <= M < 14)\n[-c X] -m A [B], where X is height of measurement starting point, A is RAD angle (0;1.4> and B is RAD angle (0;1.4>";
       printf("%s", napoveda);
       return 0;
     }
     else if(!strcmp("--tan",argv[1]) && argc == 5) {
       char *end;
       double tt = strtod(argv[2], &end);
-				if(!checkAngle(tt) || *end != '\0') {
+				if(/* !checkAngle(tt) || */*end != '\0') {
 					fprintf(stderr,"%s", "Incorrect angle value (type --help)");
 					return 1;
 				}
       int N = strtol(argv[3], &end, 10);
       char *end4;
       int M = strtol(argv[4], &end4, 10);
-        if(0 < N && N <= M && M < 14 && M != 0 && *end == '\0' && *end4 == '\0')
-          tiskniTan(&N, &M, &tt);
+        if(0 < N && N <= M && M < 14 && M != 0 && *end == '\0' && *end4 == '\0' && !isinf(tt) && !isnan(tt))
+          	tiskniTan(&N, &M, &tt);
         else {
 					fprintf(stderr, "%s", "Incorrect N / M values (type --help)");
 					return 1;
